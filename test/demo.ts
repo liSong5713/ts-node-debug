e684f4a3d2272c30fd75f2feb139b8d6a0ec3e03
rick.li <rick.li@ly.com>
2021-08-04 20:04:34 +0800

    fix conflict

--- test/demo.ts
+++ test/demo.ts
@@ -1,16 +1,10 @@
 import http from 'http'
 const test  = require('./test')
 
-const h = require('./hi')
 
 const server = http.createServer((req, res) => {
-<<<<<<< HEAD
-  console.log(h.a)
-  res.end('hello world3')
-=======
   console.log(test.value)
   res.end('hello world')
->>>>>>> develop
 })
 server.listen(3000, () => {
   console.log('server start~')
